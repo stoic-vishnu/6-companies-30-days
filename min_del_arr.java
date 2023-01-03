abaf92c83d58a5f4c0100ed229331596f81a0ad4
Mahavishnu G <stoicvishnu@gmail.com>
2023-01-03 12:47:56 +0530

    Day 1
    2344. Minimum Deletions to Make Array Divisible ( HARD )
    
    You are given two positive integer arrays nums and numsDivide.
    
    Return the min no. of deletions such that the smallest element in nums divides all the elements of numsDivide. If this is not, return -1.

---
+++ min_del_arr.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class min_del_arr {
+    public static int gcd(int c, int d) {
+        if (c == 0)
+            return d;
+        return gcd(d % c, c);
+    }
+    public static int min_delete(int[] nums, int[] ndiv) {
+        int res = ndiv[0];
+        for (int i : ndiv) {
+            res = gcd(res, i);
+        }
+        Arrays.sort(nums);
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (res % nums[i] != 0)
+                count++;
+            else {
+                break;
+            }
+        }
+        if (count == 0)
+            return -1;
+        else
+            return count;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int nums = sc.nextInt();
+        int arr1[] = new int[nums];
+        int arr2[] = new int[nums];
+        for (int i = 0; i < nums; i++) {
+            arr1[i] = sc.nextInt();
+        }
+        for (int i = 0; i < nums; i++) {
+            arr2[i] = sc.nextInt();
+        }
+        System.out.println("Minimum Deletions make arr div :" + min_delete(arr1, arr2));
+    }
+}
