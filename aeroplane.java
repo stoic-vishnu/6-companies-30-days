ac0418d70f3e8de9b44c061f576cbafb6d4f817e
Mahavishnu G <stoicvishnu@gmail.com>
2023-01-04 09:33:21 +0530

    Day 1
    n passengers board an airplane with exactly n seats. The first passenger has lost the ticket and picks a seat randomly.
    
    Return the probability that the nth person gets his own seat.
    
    Example 1:
    Input: n = 1
    Output: 1.00000
    Explanation: The first person can only get the first seat.
    
    Example 2:
    Input: n = 2
    Output: 0.50000

---
+++ aeroplane.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class aeroplane {
+    public static double nthseat(int n) {
+        if (n == 1)
+            return 1;
+        double res = 1.0 / 2.0;
+        return res;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println(nthseat(n));
+    }
+}
