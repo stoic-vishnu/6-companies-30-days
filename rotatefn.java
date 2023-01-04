94184a8c98676fbecb71abba80e1a68b8e6c6697
Mahavishnu G <stoicvishnu@gmail.com>
2023-01-04 09:52:41 +0530

    Day 2
    396. Rotate Function ( MEDIUM )
    
    You are given an integer array nums of length n, rotation function F on nums as follow:
    F(k) = 0 * arrk[0] + 1 * arrk[1] + ... + (n - 1) * arrk[n - 1].
    
    Example 1:
    
    Input: nums = [4,3,2,6]
    Output: 26
    Explanation:
    F(0) = (0 * 4) + (1 * 3) + (2 * 2) + (3 * 6) = 0 + 3 + 4 + 18 = 25
    F(1) = (0 * 6) + (1 * 4) + (2 * 3) + (3 * 2) = 0 + 4 + 6 + 6 = 16
    F(2) = (0 * 2) + (1 * 6) + (2 * 4) + (3 * 3) = 0 + 6 + 8 + 9 = 23
    F(3) = (0 * 3) + (1 * 2) + (2 * 6) + (3 * 4) = 0 + 2 + 12 + 12 = 26
    So the maximum value of F(0), F(1), F(2), F(3) is F(3) = 26.

---
+++ rotatefn.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class rotatefn {
+    public static int rotatefunct(int[] nums) {
+        int max = 0, ans = 0, sum = 0;
+        int h = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            ans += nums[i] * i;
+        }
+        for (int j = nums.length - 1; j > 0; j--) {
+            // max = formula(ans, sum, nums.length, nums[j]);
+            int m = 0;
+            m = ans + sum - nums.length * nums[j];
+            max = Math.max(max, m);
+            ans = m;
+            // return max;
+
+        }
+        System.out.println(max);
+        return max;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr1[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr1[i] = sc.nextInt();
+        }
+        System.out.println("Max Rotate Fn : "+rotatefunct(arr1));
+
+    }
+}
